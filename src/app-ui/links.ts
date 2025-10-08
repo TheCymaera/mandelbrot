af905648e1ac0e51064ac9b5fdbced0887972029
Cymaera <69355340+TheCymaera@users.noreply.github.com>
2025-10-09 01:09:46 +0800

    Change YouTube link

--- src/app-ui/links.ts
+++ src/app-ui/links.ts
@@ -1,5 +1,5 @@
 export const githubRepositoryLink = `https://github.com/TheCymaera/mandelbrot`;
 
-export const youtubeEmbedLink = `https://www.youtube.com/embed/MTIdwbAvzjg`;
+export const youtubeEmbedLink = `https://www.youtube.com/embed/aLR4YpTnM6c`;
 
 export const homeLink = "https://heledron.com";
